dc79e302192936be6b35ef8a8241425815e5e7b3
janekbechtel <janek.bechtel@student.kit.edu>
2019-08-15 11:42:35 +0200

    Remove comments and unify naming of output files

--- makeCorrectionsWorkspace_legacy_2017.py
+++ makeCorrectionsWorkspace_legacy_2017.py
@@ -143,40 +143,6 @@ for task in desyHistsToWrap:
 for t in ['trg_MuTau_Mu20Leg_desy','trg_SingleMu_Mu24ORMu27_desy']:
     w.factory('expr::m_%s_ratio("@0/@1", m_%s_data, m_%s_mc)' % (t, t, t))
 
-
-### DESY electron & muon tag and probe results
-#loc = 'inputs/2017/LeptonEfficiencies'
-#
-## electron triggers
-#desyHistsToWrap = [
-#    (loc+'/Electron/Run2017/Electron_EleTau_Ele24.root',           'MC', 'e_trg_EleTau_Ele24Leg_desy_mc'),
-#    (loc+'/Electron/Run2017/Electron_EleTau_Ele24.root',           'Data', 'e_trg_EleTau_Ele24Leg_desy_data'),
-#    (loc+'/Electron/Run2017/Electron_Ele32orEle35.root',           'MC', 'e_trg_SingleEle_Ele32OREle35_desy_mc'),
-#    (loc+'/Electron/Run2017/Electron_Ele32orEle35.root',           'Data', 'e_trg_SingleEle_Ele32OREle35_desy_data')
-#]
-#
-#for task in desyHistsToWrap:
-#    wsptools.SafeWrapHist(w, ['e_pt', 'expr::e_abs_eta("TMath::Abs(@0)",e_eta[0])'],
-#                          wsptools.ProcessDESYLeptonSFs_2017(task[0], task[1], task[2]), name=task[2])
-#
-#for t in ['trg_EleTau_Ele24Leg_desy','trg_SingleEle_Ele32OREle35_desy']:
-#    w.factory('expr::e_%s_ratio("@0/@1", e_%s_data, e_%s_mc)' % (t, t, t))
-#
-## muon triggers
-#desyHistsToWrap = [
-#    (loc+'/Muon/Run2017/Muon_MuTau_IsoMu20.root',           'MC', 'm_trg_MuTau_Mu20Leg_desy_mc'),
-#    (loc+'/Muon/Run2017/Muon_MuTau_IsoMu20.root',           'Data', 'm_trg_MuTau_Mu20Leg_desy_data'),
-#    (loc+'/Muon/Run2017/Muon_IsoMu24orIsoMu27.root',           'MC', 'm_trg_SingleMu_Mu24ORMu27_desy_mc'),
-#    (loc+'/Muon/Run2017/Muon_IsoMu24orIsoMu27.root',           'Data', 'm_trg_SingleMu_Mu24ORMu27_desy_data')
-#]
-#
-#for task in desyHistsToWrap:
-#    wsptools.SafeWrapHist(w, ['m_pt', 'expr::m_abs_eta("TMath::Abs(@0)",m_eta[0])'],
-#                          wsptools.ProcessDESYLeptonSFs_2017(task[0], task[1], task[2]), name=task[2])
-#
-#for t in ['trg_MuTau_Mu20Leg_desy','trg_SingleMu_Mu24ORMu27_desy']:
-#    w.factory('expr::m_%s_ratio("@0/@1", m_%s_data, m_%s_mc)' % (t, t, t))
-#
 ### KIT electron/muon tag and probe results
 
 # triggr SFs Muons from KIT
@@ -601,5 +567,5 @@ for task in histsToWrap:
 w.importClassCode('CrystalBallEfficiency')
 
 #w.Print()
-w.writeToFile('output/htt_scalefactors_2017.root')
+w.writeToFile('output/htt_scalefactors_legacy_2017.root')
 w.Delete()
